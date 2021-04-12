72967b9d0ec7232ceafc664381253b7df6fab122
Natalie <theliteshado@icloud.com>
2021-04-12 16:46:43 -0400

    main

--- server.js
+++ server.js
@@ -4,16 +4,24 @@ const app = express()
 const routesReport = require('rowdy-logger').begin(app)
 
 const path = require('path')
+const replaceInFile = require('replace-in-file')
+
 
 app.get('/', (req, res) => {
   const filepath = path.join(__dirname, 'index.html')
   res.sendFile(filepath)
 })
 
-app.get('/main.js', (req, res) => {
+app.get('/main.js', async (req, res) => {
   const filepath = path.join(__dirname, 'main.js')
-  res.sendFile(filepath)
-})
+
+if (process.env.NODE_ENV ==='production') {
+  await replaceInFile({
+      files: filepath,
+      from: 'http://localhost:3001',
+      to: 'https://lyrically-backend.herokuapp.com/'
+  })
+}
 
 app.get('/style.css', (req, res) => {
   const filepath = path.join(__dirname, 'style.css')
