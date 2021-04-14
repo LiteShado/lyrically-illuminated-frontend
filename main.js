52f688d951215cf38409e826d141b04a00f46b89
Natalie <theliteshado@icloud.com>
2021-04-14 09:58:13 -0400

    hcrud fix

--- main.js
+++ main.js
@@ -226,24 +226,24 @@ document.querySelector('#logout-link').addEventListener('click', async (event) =
             localStorage.removeItem('userId')
 
     console.log(response)
+    document.querySelector('#profile-link').classList.remove('active')
+    document.querySelector('#profile-link').classList.add('hidden')
+    showLoggedOut()
+    document.querySelector('#signup-link').classList.remove('hidden')
+    document.querySelector('#signup-link').classList.add('active')
+
+
     // showLoggedOut()
     // authCheck()
 }   catch (error) {
     console.log(error)
+    document.querySelector('#profile-link').classList.remove('active')
+    document.querySelector('#profile-link').classList.add('hidden')
+    showLoggedOut()
+    document.querySelector('#signup-link').classList.remove('hidden')
+    document.querySelector('#signup-link').classList.add('active')
 }
 })
-
-// const authCheck = () => {
-//     const userId = localStorage.getItem('userId')
-//     if (userId) {
-//         let userEmail = document.querySelector('#userEmail')
-//         let usersEmail = localStorage.getItem('resemail')
-//         userEmail.innerText = usersEmail
-//     } else {
-//         showLoggedOut()
-//     }
-// }
-// authCheck()
 
 document.querySelector('#profile-link').addEventListener('click', async (event) => {
     event.preventDefault()
@@ -277,166 +277,25 @@ document.querySelector('#profile-link').addEventListener('click', async (event) 
 //     console.log(error)
 // }
 // })
-
-document.querySelector('#delete').addEventListener('submit', async (event) => {
+const deleted = document.querySelector('#delete')
+deleted.addEventListener('submit', async (e) => {
     event.preventDefault()
-    // try {
-        // let user = await models.user.destroy({
-            where: {
-                id: localStorage.removeItem('userId', userId)
-                id: localStorage.clear()
-
-            }
-        // ({
-    console.log(userId)
-    // showLoggedOut()
-// }   catch (error) {
-//
-    // console.log(error)
-        })
-        console.log(localStorage)
-
-// // SHOW ACCOUNT
-// const showAccount = () => {
-//     userAccount.classList.add('hidden')
-//     userSignUp.classList.add('hidden')
-//     loginForm.classList.add('hidden')
-//     success.classList.add('hidden')
-//     saved.classList.remove('hidden')
-//     newInput.classList.add('hidden')
-//     newInputForm.classList.add('hidden')
+    console.log("delete")
+    try {
+        const userId = localStorage.getItem('userId')
+        // const deleteUser = (userId, () => {
+        let res = await axios.delete(`http://localhost:3001/user/${userId}/delete`)
+        console.log(res)
+        localStorage.removeItem('userId', userId)
+        localStorage.clear()
+        showLoggedOut()
+    } catch (error) {
+        console.log(error)
+    }
+})
 
 
-//     let userName = document.querySelector('.userName')
-//     let usersName = localStorage.getItem('userName')
-//     if(usersName !== undefined) {
-//         userName.innerText = usersName
-//     }
-// }
 
-//     try {
-//         const response = await axios.post('http://localhost:3001/user', {
-//             name: name,
-//             email: email,
-//             password: password,
-//         })
-//         console.log(response)
-
-//         const userId = response.data.user.id
-//         console.log(userId)
-
-//         const userName = response.data.user.name
-//         console.log(userName)
-
-//         localStorage.setItem('userId', userId)
-//         localStorage.setItem('userName', userName)
-//         localStorage.setItem('userMood', userMood)
-//         localStorage.setItem('userTag', userTag)
-//         getAllLocations()
-//         switchToLogin()
-
-//     } catch (error) {
-//         console.log(error)
-//     }
-// })
-
-// const loginForm = document.querySelector('.userLogin')
-// loginForm.addEventListener('submit', async (e) => {
-//     e.preventDefault()
-//     const email = document.querySelector('#userLoginEmail').value
-
-//     const password = document.querySelector('#userLoginPassword').value
-//     try {
-//         const response = await axios.post('http://localhost:3001/user/login', {
-//             email: email,
-//             password: password
-//     })
-
-//     console.log(response)
-
-//     const userId = response.data.id
-//     const userName = response.data.name
-//     const userMood = response.data.mood
-//     const userTag = response.data.tag
-//     localStorage.setItem('userName', userName)
-//     localStorage.setItem('userId', userId)
-//     localStorage.setItem('userMood', userMood)
-//     localStorage.setItem('userTag', userTag)
-//     getAllLocations()
-//     switchToLogin()
-
-//     } catch (error) {
-//         console.log(error)
-//         alert(error)
-//     }
-// })
-
-// // LOG IN OR LOG OUT
-
-// const loginButton = document.querySelector('.loginButton')
-// loginButton.addEventListener('click', () => {
-//     switchToLogin()
-// })
-
-// const logoutButton = document.querySelector('.signOutButton')
-// logoutButton.addEventListener('click', () => {
-//     logout()
-// })
-// const logout = () => {
-//     localStorage.clear()
-//     authCheck()
-// }
-
-// // Retrieve Music
-
-// let illuminate = document.querySelector('.success')
-// illuminate.addEventListener('submit', async(e) => {
-//     e.preventDefault()
-//     try {
-//         const mood = document.querySelector('#successMood').value
-//         console.log(mood)
-
-//         const tag = document.querySelector('#successTag').value
-//         console.log(tag)
-
-//         const res = await axios.post(`http://localhost:3001/location/search/${searchBar}`)
-//         console.log(res.data)
-
-//         showResults(res.data)
-//         searchForm.reset()
-//     } catch (error) {
-//         console.log(error)
-//     }
-// })
-
-// resultsId = null
-// const showResults = (data) => {
-//     resultsId = data.id
-//     let searchResults = document.querySelector('.searchResults')
-//     searchResults.classList.remove('hidden')
-
-
-//     let resultsLocation = document.querySelector('.resultsLocation')
-//     resultsLocation.innerText = data.name
-
-//     let resultsWeather = document.querySelector('.resultsWeather')
-//     resultsWeather.innerText = data.weather[0].main
-//     let resultsWeather2 = document.querySelector('.resultsWeather2')
-//     resultsWeather2.innerText = data.weather[0].description
-
-//     let resultsFeelsLike = document.querySelector('.resultsFeelsLike')
-//     resultsFeelsLike.innerText = `Feels like: ${data.main.feels_like}`
-//     let resultsHumidity = document.querySelector('.resultsHumidity')
-//     resultsHumidity.innerText = `Humidity: ${data.main.humidity}`
-//     let resultsMax = document.querySelector('.resultsMax')
-//     resultsMax.innerText = `Max: ${data.main.temp_max}`
-
-//     let resultsMin = document.querySelector('.resultsMin')
-//     resultsMin.innerText = `Min: ${data.main.temp_min}`
-
-// }
-
-// const saveSearch = document.querySelector('.saveSearch')
 // saveSearch.addEventListener('click', async (e) => {
 //     e.preventDefault()
 
@@ -449,86 +308,18 @@ document.querySelector('#delete').addEventListener('submit', async (event) => {
 //     } catch (error) {
 //         console.log(res)
 //     }
-// })
-// let closeSearch = document.querySelector('.closeSearch')
-// let closeSearchFunc = () => {
-//     let searchResults = document.querySelector('.searchResults')
-//     searchResults.classList.add('hidden')
-// }
-// closeSearch.addEventListener('click', () => {
-//   closeSearchFunc()
-// })
-
-
-// //SHOW SAVED LOCATIONS
 
 
 
-// const getAllLocations = async () => {
-//     let userId = localStorage.getItem('userId')
-//     let locationsContainer = document.querySelector('.locationsContainer')
-//     while(locationsContainer.firstChild) {
-//         locationsContainer.firstChild.remove()
-//     }
-//     if(userId) {
-//         let res = await axios.get(`http://localhost:3001/user/${userId}/getlocations`)
-//         console.log(res.data.locations)
-//         addLocations(res.data.locations)
-//     }
-// }
-
-// let deleteSingle = async (locationId) => {
-//     let userId = localStorage.getItem('userId')
-//     let res = await axios.delete(`http://localhost:3001/user/${userId}/delete/${locationId}`)
-//     console.log(res)
-//     getAllLocations()
-// }
-// const addLocations = async (data) => {
-//     let locationsContainer = document.querySelector('.locationsContainer')
-//     console.log(data)
-//     while(locationsContainer.firstChild) {
-//         locationsContainer.firstChild.remove()
-//     }
-//     for(let location of data) {
-//         let res = await axios.get(`http://localhost:3001/location/search/${location.locationId}`)
-//         console.log(res.data)
-//         console.log(location.id)
-
-//         let div = document.createElement('div')
-//         let h1 = document.createElement('h1')
-//         let temp = document.createElement('p')
-//         let weather = document.createElement('p')
-//         temp.classList.add('singleTemp')
-//         weather.classList.add('singleWeather')
-//         weather.innerText = res.data.weather[0].main
-//         temp.innerText = res.data.main.temp
-//         div.classList.add('singleLocation')
-//         h1.innerText = res.data.name
-//         div.append(h1)
-//         div.append(temp)
-//         div.append(weather)
-//         let deleteButton = document.createElement('button')
-//         deleteButton.classList.add('deleteSingle')
-//         deleteButton.innerText = 'delete'
-//         deleteButton.addEventListener('click', (e) => {
-//             e.preventDefault()
-//             deleteSingle(location.id)
-//         })
-//         div.append(deleteButton)
-//         locationsContainer.append(div)
-//     }
-
-// }
-
-// const authCheck = () => {
-//     const userId = localStorage.getItem('userId')
-//     if (userId) {
-//         let userName = document.querySelector('.userName')
-//         let usersName = localStorage.getItem('userName')
-//         userName.innerText = usersName
-//         getAllLocations()
-//         switchToDash()
-//     } else {
-//         switchToLogin()
-//     }
-// }
+const authCheck = () => {
+    const userId = localStorage.getItem('userId')
+    if (userId) {
+        let userName = document.querySelector('.userName')
+        let usersName = localStorage.getItem('userName')
+        userName.innerText = usersName
+        getAllLocations()
+        showLoggedIn()
+    } else {
+        showLoggedOut()
+    }
+}
